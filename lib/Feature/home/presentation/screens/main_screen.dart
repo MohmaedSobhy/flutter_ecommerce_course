2ed19a57641d1222d1c7e732a279123d5e67908b
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-12-22 23:11:22 +0200

    bottom navigation screen

---
+++ lib/Feature/home/presentation/screens/main_screen.dart
@@ -0,0 +1,57 @@
+import 'package:ecommerce_app/Feature/favourite/presentation/screens/favourite_screen.dart';
+import 'package:ecommerce_app/Feature/home/presentation/screens/home_screen.dart';
+import 'package:ecommerce_app/Feature/home/presentation/widgets/custome_button_navigation.dart';
+import 'package:ecommerce_app/Feature/profile/presentation/screens/profile_screen.dart';
+import 'package:ecommerce_app/Feature/store/presentation/screens/store_screen.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  late PageController pageController;
+  int currentScreen = 0;
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: PageView(
+          controller: pageController,
+          onPageChanged: onPageChanged,
+          children: const [
+            HomeScreen(),
+            StoreScreen(),
+            FavouriteScreen(),
+            ProfileScreen(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: CustomeButtonNavigation(
+        onButtonNavigationTap: onButtonNavigationTap,
+      ),
+    );
+  }
+
+  onPageChanged(int page) {
+    setState(() {
+      currentScreen = page;
+    });
+  }
+
+  onButtonNavigationTap(int page) {
+    setState(() {
+      pageController.jumpToPage(page);
+      currentScreen = page;
+    });
+  }
+}
